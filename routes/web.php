6909193428d29e8b27e105f13ebb3bb81124081c
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-09-10 20:07:17 +0700

    feat: add user controller

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/user', [UserController::class, 'user_profile']);
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user', [PostController::class, 'user_profile']);
+Route::get('/post', [PostController::class, 'user_profile']);
+
+Route::resource('/user', UserController::class);
